3e60f3e5dcc42d2d7fc960744f940f58c891a8a7
taecongs <bks0927w@gmail.com>
2022-12-19 16:55:03 +0900

    Study : React Section 3

--- react/221219/react-complete-guide/src/App.js
+++ react/221219/react-complete-guide/src/App.js
@@ -2,10 +2,20 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+  const expense = [
+    {id : 'e1', title : 'Toilet Paper', amount : 94.12, date : new Date(2020, 7, 14)},
+    {id : 'e2', title : 'New TV', amount : 799.49, date : new Date(2021, 2, 12)},
+    {id : 'e3', title : 'Car Insurance', amount : 294.67, date : new Date(2021, 2, 28)},
+    {id : 'e4', title : 'New Phone', amount : 564.57, date : new Date(2022, 5, 28)},
+  ];
+
   return (
-    <div>
-      <ExpenseItem />
-    </div>
+    <>
+      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date} />
+      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date} />
+      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date} />
+      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date} />
+    </>
   );
 }
 
--- react/221219/react-complete-guide/src/components/ExpenseItem.js
+++ react/221219/react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2022, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+const ExpenseItem = (props) => {
+    // const expenseDate = new Date(2022, 2, 28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
 
     return(
         <div className='expense-item'>
             {/* 모든 date 객체에 사용가능한 내장 메소드 : toISOString을 사용하여 문자열로 출력 */}
-            <div>{expenseDate.toISOString}</div>
+            <div>{props.date.toISOString()}</div>
 
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
