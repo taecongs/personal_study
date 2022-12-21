7428109b96512f1f886a1eb02935a0817fdbc1e7
taecongs <bks0927w@gmail.com>
2022-12-21 16:23:19 +0900

    Study : Section 4

--- react/221221/section4/src/components/NewExpense/ExpenseForm.js
+++ react/221221/section4/src/components/NewExpense/ExpenseForm.js
@@ -51,22 +51,43 @@ const ExpenseForm = () => {
         // });
     }
 
+
+    // 페이지가 새로고침되는 이유는?
+    // 브라우저는 폼이 제출될 때마다 웹 페이지를 호스팅하고 있는 서버에 요청을 보내기 때문이다.
+    const submitHandler = (e) => {
+        // 새로고침을 막아주는 기본 자바스크립트 메서드이다.
+        e.preventDefault();
+
+        const expenseData = {
+            title : enterTitle,
+            amount : enterAmount,
+            date : new Date(enterDate)
+        };
+
+        console.log(expenseData);
+
+        // 버튼 클릭 시 초기화 
+        setEnterTitle('');
+        setEnterAmount('');
+        setEnterDate('');
+    }
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' value={enterTitle} onChange={titleChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input type='number' min='0.01' step='0.01' value={enterAmount} onChange={amountChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2022-01-01' max='2025-12-31' onChange={dateChangeHandler}  />
+                    <input type='date' min='2022-01-01' max='2025-12-31' value={enterDate} onChange={dateChangeHandler}  />
                 </div>
             </div>
 
